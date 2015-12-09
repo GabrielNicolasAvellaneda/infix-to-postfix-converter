6dd1f5703b8219184b0caff4bea4b9e9cba29384
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-09 17:16:47 -0300

    Convert expressions of two terms.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- python/infixtopostfix.py
+++ python/infixtopostfix.py
@@ -1,10 +1,20 @@
 import unittest
+import string
 
 def infix_to_postfix(infix_expression):
-    result = ""
+    result = []
+    operators_stack = []
+    operators = "+-/*"
     for token in infix_expression.split():
-        result += token 
-    return result
+        if token in operators:
+            operators_stack.append(token)
+        else:
+            result.append(token)
+
+    while len(operators_stack) > 0:
+        result.append(operators_stack.pop())
+
+    return string.join(result, " ")
 
 class InfixToPostfixConverterTest(unittest.TestCase):
     
@@ -13,6 +23,10 @@ class InfixToPostfixConverterTest(unittest.TestCase):
         self.assertEqual("1", result)
         result = infix_to_postfix("100")
         self.assertEqual("100", result)
+
+    def test_sum_of_two_numbers(self):
+        result = infix_to_postfix("1 + 3")
+        self.assertEqual("1 3 +", result)
 
 if __name__ == "__main__":
     unittest.main()
